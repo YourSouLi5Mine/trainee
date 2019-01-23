cd28d7966715234f4987e6b867d27f29a5e9bab3
Eduardo Mendoza Garibo <eduardo mendoza@tiempodev.local>
2019-01-23 14:01:39 -0600

    Create employee class

---
+++ employee.rb
@@ -0,0 +1,16 @@
+class Employee
+	
+	attr_reader :name, :title
+	attr_writer :earnings
+
+	def initialize(name, title)
+		@name = name
+		@title = title
+		@earnings = 0
+	end
+	
+	def pay(amount)
+		@earnings += amount
+		"Payment #{amount} was successful current earnings is #{@earnings}
+	end
+end
